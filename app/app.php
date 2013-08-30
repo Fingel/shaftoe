b1c9da7c392c4e50e4067dc02d7e6a65e2bf0319
Austin Riba <root@austinriba.com>
2013-08-30 00:01:15 -0700

    add some validation, cleanup

--- app/app.php
+++ app/app.php
@@ -17,11 +17,26 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 $app->post('/key', function (Request $request) use ($app){
     $email = $request->get('email');
     $key = $request->get('publickey');
+    if(empty($email) || empty($key)){
+        $ret = array (
+            "msg"=>"Missing input. Please provide a valid email address and public key.",
+            "success"=>false
+        );
+        return $app->json($ret);
+    }
     $sql = "INSERT INTO pgweb.pgkeys (email, pgpkey) VALUES(?,?)";
     $stmt = $app['db']->prepare($sql);
     $stmt->bindValue(1, $email);
     $stmt->bindValue(2, $key);
-    $stmt->execute();
+    try {
+        $stmt->execute();
+    } catch(Exception $e){
+        $ret = array (
+            "msg"=>"Duplicate email!",
+            "success"=>false
+        );
+        return $app->json($ret);
+    }
     error_log($key);
     
     $ret = array (
@@ -49,13 +64,31 @@ $app->get('/key/{email}', function($email) use($app){
 $app->post('/encrypt', function(Request $request) use ($app){
     $email = $request->get('email');
     $message = $request->get('message');
+    if(empty($email) || empty($message)){
+        $ret = array (
+            "msg"=>"Missing input. Please provide a valid email address and plaintext message.",
+            "success"=>false
+        );
+        return $app->json($ret);
+    }
     $sql = "SELECT pgpkey from pgweb.pgkeys WHERE email = ?";
     $key = $app['db']->fetchColumn($sql, array((string) $email));
     $key=  str_replace("\r", '', $key);   
     error_log($email);
     $gpg = new GPG();
     $pub_key = new GPG_Public_Key($key);
-    $encrypted = $gpg->encrypt($pub_key, "howdy");
+    if(empty($pub_key->user)){
+        $ret = array (
+            "msg"=>"Oh no! Encrypting of the message failed. Make sure your public key is correct. Ive deleted it for you.",
+            "success"=>false
+        );
+        $sql = "DELETE from pgweb.pgkeys WHERE email = ?";
+        $stmt = $app['db']->prepare($sql);
+        $stmt->bindValue(1, $email);
+        $stmt->execute();
+        return $app->json($ret);
+    }
+    $encrypted = $gpg->encrypt($pub_key, $message);
     $ret = array (
         "msg"=>$encrypted,
         "success"=>true
